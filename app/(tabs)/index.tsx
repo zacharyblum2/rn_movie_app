c2bdd788e25bbcb38d886e911361cfb62415f6aa
Zachary Blum <blum.zachary1@gmail.com>
2025-03-16 18:25:17 -0400

    Add SafeAreaView to all screens.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,10 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center">
+    <SafeAreaView className="flex-1 justify-center items-center">
       <Text className="text-5xl font-bold text-light-100">Hello World</Text>
-    </View>
+    </SafeAreaView>
   );
 }
